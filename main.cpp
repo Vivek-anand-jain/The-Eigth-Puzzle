0dc2446f6a1775b9a93b54b2adf812525da33dad
Vivek Jain <jain.vivek.anand@gmail.com>
2022-05-12 16:00:11 -0700

    add default puzzles

--- main.cpp
+++ main.cpp
@@ -2,6 +2,36 @@
 
 using namespace std;
 
+vector<vector<int>> DefaultPuzzle() {
+  int done = 1;
+  int puzzle;
+  do {
+    done = 1;
+    cout << "Enter one of the depth: 0, 2, 4, 8, 12, 16, 20, 24" << endl;
+    cin >> puzzle;
+    switch (puzzle) {
+      case 0:
+        return {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
+      case 2:
+        return {{1, 2, 3}, {4, 5, 6}, {0, 7, 8}};
+      case 4:
+        return {{1, 2, 3}, {5, 0, 6}, {4, 7, 8}};
+      case 8:
+        return {{1, 3, 6}, {5, 0, 2}, {4, 7, 8}};
+      case 12:
+        return {{1, 3, 6}, {5, 0, 7}, {4, 8, 2}};
+      case 16:
+        return {{1, 6, 7}, {5, 0, 3}, {4, 8, 2}};
+      case 20:
+        return {{7, 1, 2}, {4, 8, 5}, {6, 3, 0}};
+      case 24:
+        return {{0, 7, 2}, {4, 6, 1}, {3, 5, 8}};
+      default:
+        done = 0;
+    }
+  } while (done != 1);
+}
+
 int main() {
   int n = 3, temp;
   int puzzleMode = 1;
@@ -17,7 +47,7 @@ int main() {
     cin >> puzzleMode;
     switch (puzzleMode) {
       case 1:
-        cout << "Choose level of difficulty" << endl;
+        vec = DefaultPuzzle();
         break;
       case 2:
         cout << "Enter 3 * 3 puzzle, zero means blank" << endl;
@@ -38,7 +68,8 @@ int main() {
     }
   } while (done != 1);
 
-  cout << "User input" << endl;
-  // User
+  Board board(vec);
+  board.Print();
+
   return 0;
 }
