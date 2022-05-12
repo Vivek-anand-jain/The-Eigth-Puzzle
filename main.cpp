a2b6d8ce32590986a6d632a4ae24f96a23df7b6a
Vivek Jain <jain.vivek.anand@gmail.com>
2022-05-12 16:45:06 -0700

    Add algorithm choice

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 int main() {
   int n = 3, temp;
   int puzzleMode = 1;
+  int algorithm = 1;
   vector<vector<int>> vec;
   cout << "Welcome to my CS205 8-Puzzle Solver. Type '1' to use a default "
           "puzzle, or '2' to create your own."
@@ -42,10 +43,29 @@ int main() {
   } while (done != 1);
 
   Board board(vec);
+  cout << "Solving following puzzle: " << endl;
   board.Print();
 
+  cout << endl;
+
+  cout << "Select algorithm: \n'1' for Uniform Cost Search\n'2' for A* with "
+          "Misplaced Tile Heuristic\n'3' for A* with Manhattan Distance "
+          "Heuristic"
+       << endl;
+  do {
+    done = 1;
+    cin >> algorithm;
+    if (algorithm < 1 || algorithm > 3) {
+      cout << "Invalid choice, Type \n'1' for Uniform Cost Search\n'2' for "
+              "A* with Misplaced Tile Heuristic\n'3' A* with Manhattan "
+              "Distance Heuristic"
+           << endl;
+      done = 0;
+    }
+  } while (done != 1);
+
   Solver solver;
-  solver.Solve(board);
+  solver.Solve(board, algorithm);
 
   return 0;
 }
